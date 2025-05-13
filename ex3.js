4f5ce01a66e34c8748b26a6a069965316dcb1fad
unknown <joaovitordiasdejesus1234@gmail.com>
2025-05-13 13:30:44 -0300

    exercicio1

--- ex3.js
+++ ex3.js
@@ -1,11 +1,11 @@
 let totalCompra = 0;
-    let valor = prompt("Digite o valor da compra (0 para encerrar):");
+let valor = parseFloat(prompt("Digite o valor da compra (0 para encerrar):"));
 
-    while (valor != 0) {
-      if (valor > 0) {
-        totalCompra = totalCompra + parseFloat(valor);
-      }
-      valor = prompt("Digite o valor da compra (0 para encerrar):");
-    }
+while (valor > 0 || valor < 0) {
+  if (valor > 0) {
+    totalCompra = totalCompra + valor; 
+  }
+  valor = parseFloat(prompt("Digite o valor da compra (0 para encerrar):"));
+}
 
-    alert("Total da compra: R$ " + totalCompra.toFixed(2));
+alert("Total da compra: R$ " + totalCompra.toFixed(2));
